d8c6dd195eb8d95f7069f0bcd210de0cdd494fa0
Henry Lin <henrylin58@gmail.com>
2020-05-22 01:23:56 -0700

    added gaming window and rocket

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import sys
 import pygame 
 
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion: 
     # Overall all class to manage game assets and behavior
@@ -15,6 +16,8 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
 
         # Set the background color.
         self.bg_color = (230, 230, 230)
@@ -29,6 +32,7 @@ class AlienInvasion:
         
             #Redraw the screen during each pass through the loop
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
                 
             # This makes the most recently drawn screen available
             pygame.display.flip()
