2121b91fe576cb7d4a21cda115db7d5028cb20ab
Henry Lin <henrylin58@gmail.com>
2020-05-23 01:29:38 -0700

    enabled easier access to ship's speed

--- ship.py
+++ ship.py
@@ -6,6 +6,7 @@ class Ship:
     def __init__(self, ai_game):
         # Initialize the ship and set its starting position.
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # Load the ship image and get its rect
@@ -14,6 +15,9 @@ class Ship:
 
         # start each new ship at the bottom center of the screen
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # store a decimal value for the ship's horizontal position
+        self.x = float(self.rect.x)
 
         # Movement flag
         self.moving_right = False
