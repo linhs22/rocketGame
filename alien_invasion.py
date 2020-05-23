90275ce252103a17815e48e4bec0a56e7cffbcc2
Henry Lin <henrylin58@gmail.com>
2020-05-23 01:11:50 -0700

    have the ship move when right arrow is pressed

--- alien_invasion.py
+++ alien_invasion.py
@@ -33,6 +33,15 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    # Move the ship to the right.
+                    self.ship.moving_right = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                    
+
     
     def _update_screen(self):
         #Redraw the screen during each pass through the loop
